73ec023edb6923017cf556ca32205fd3b75ed020
ngrudenko <ngrudenko@yamoney.ru>
2018-12-20 20:25:16 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-// export {AwesomeShit} from './components/AwesomeShit';
-// language: node_js
-// node_js:
-// 	- "8"
-//
-// jobs:
-// 	include:
-// 		- stage: test
-// script: npm test
-// script: npm run lint
-// - stage: publish
-// deploy:
-// 	provider: npm
-// email:
-// 	on:
-// 		tags: tags
+export {AwesomeShit} from './components/AwesomeShit';
